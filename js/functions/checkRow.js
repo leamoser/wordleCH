24a6d96599e12636a9fa20b9f0fcff3df77ecfba
Lea Moser <lea.moser@stud.fhgr.ch>
2022-03-22 18:39:04 +0100

    minor updates

--- js/functions/checkRow.js
+++ js/functions/checkRow.js
@@ -5,32 +5,43 @@ import restart from "../elements/restartButton.js";
 import generateEmojiResult from "./generateEmojiResult.js";
 
 function checkRow(wrapper){
+
     const ROWID = wrapper.dataset.rowid
     const FIELDS = document.querySelectorAll(`.row_${ROWID}`)
     const WORD = localStorage.getItem('word')
+
     let numberOfCorrectLetters = 0
-    for (let i = 0; i < WORD.length; i++){
-        let correctLetter = WORD.toUpperCase().includes(FIELDS[i].value.toUpperCase()) && FIELDS[i].value !== ''
-        let correctLetterAtCorrectPlace = FIELDS[i].value.toUpperCase() === WORD[i].toUpperCase()
-        if(correctLetter && correctLetterAtCorrectPlace) {
-            FIELDS[i].style.background = variables.colors.positive
-            FIELDS[i].setAttribute('data-state', 'positive')
-            numberOfCorrectLetters++
-        }else if(correctLetter && !correctLetterAtCorrectPlace){
-            FIELDS[i].style.background = variables.colors.warning
-            FIELDS[i].setAttribute('data-state', 'warning')
-        }else{
-            FIELDS[i].style.background = variables.colors.negative
-            FIELDS[i].setAttribute('data-state', 'negative')
+    let allFieldsFilled = true
+    FIELDS.forEach(field => {
+        if(field.value === '') { allFieldsFilled = false }
+    })
+
+    if(allFieldsFilled){
+        for (let i = 0; i < WORD.length; i++){
+            let correctLetter = WORD.toUpperCase().includes(FIELDS[i].value.toUpperCase()) && FIELDS[i].value !== ''
+            let correctLetterAtCorrectPlace = FIELDS[i].value.toUpperCase() === WORD[i].toUpperCase()
+            if(correctLetter && correctLetterAtCorrectPlace) {
+                FIELDS[i].style.background = variables.colors.positive
+                FIELDS[i].setAttribute('data-state', 'positive')
+                numberOfCorrectLetters++
+            }else if(correctLetter && !correctLetterAtCorrectPlace){
+                FIELDS[i].style.background = variables.colors.warning
+                FIELDS[i].setAttribute('data-state', 'warning')
+            }else{
+                FIELDS[i].style.background = variables.colors.negative
+                FIELDS[i].setAttribute('data-state', 'negative')
+            }
+            FIELDS[i].setAttribute('disabled', 'true')
         }
-        FIELDS[i].setAttribute('disabled', 'true')
+
+        if(numberOfCorrectLetters < variables.numberOfLetters){
+            createWordleRow(variables.numberOfLetters,variables.container,ROWID + 1)
+        }else{
+            generateEmojiResult()
+            placeElements(wrapper.parentElement, [restart])
+        }
     }
-    if(numberOfCorrectLetters < variables.numberOfLetters){
-        createWordleRow(variables.numberOfLetters,variables.container,ROWID + 1)
-    }else{
-        generateEmojiResult()
-        placeElements(wrapper.parentElement, [restart])
-    }
+
 }
 
 export default checkRow
--- js/functions/modifyInputField.js
+++ js/functions/modifyInputField.js
@@ -1,28 +1,41 @@
 import variables from "../variables.js";
 import checkRow from "./checkRow.js";
 
+function focusNext(rowId,index){
+    if(index < variables.numberOfLetters){
+        const NEXT_ELEMENT = document.querySelector(`#row_${rowId}_${index + 1}`)
+        NEXT_ELEMENT.focus()
+    }
+}
+function focusPrev(rowId,index){
+    if(index !== 1){
+        const PREV_ELEMENT = document.querySelector(`#row_${rowId}_${index - 1}`)
+        PREV_ELEMENT.focus()
+    }
+}
+
 function modifyInputField(field) {
     const INDEX = parseInt(field.dataset.index)
     const ROWID = parseInt(field.dataset.rowid)
 
-    field.addEventListener('keyup', (e) => {
+    field.addEventListener('input', (e) => {
         //wenn feld ausgefüllt
-        if (field.value.length >= 1 && INDEX < variables.numberOfLetters) {
-            const NEXT_ELEMENT = document.querySelector(`#row_${ROWID}_${INDEX + 1}`)
-            NEXT_ELEMENT.focus()
+        if (field.value.length >= 1) {
+            focusNext(ROWID,INDEX);
         }
-        //wenn letztes feld gefüllt und dann enter
-        if(field.value.length >= 1 && INDEX === variables.numberOfLetters){
-            if(e.keyCode === 13) {
-                checkRow(field.parentElement)
-            }
+    })
+    field.addEventListener('keyup', (e) => {
+        //wenn enter
+        if(e.keyCode === 13) {
+            checkRow(field.parentElement)
         }
-        //wenn feld leer und zurück und nicht im ersten Feld
-        if(INDEX !== 1){
-            if(e.keyCode === 8){
-                const PREV_ELEMENT = document.querySelector(`#row_${ROWID}_${INDEX - 1}`)
-                PREV_ELEMENT.focus()
-            }
+        //wenn back oder arrow back
+        if(e.keyCode === 8 || e.keyCode === 37){
+            focusPrev(ROWID,INDEX);
+        }
+        //wenn arrow front
+        if(e.keyCode === 39){
+            focusNext(ROWID,INDEX)
         }
     })
 }
