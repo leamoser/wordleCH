eac75f9d6f25fe59d23d4ef1b66808789ef9dfcc
Lea Moser <lea.moser@stud.fhgr.ch>
2022-03-22 19:02:07 +0100

    update dark/lightmode

--- js/functions/toggleColorMode.js
+++ js/functions/toggleColorMode.js
@@ -2,12 +2,18 @@ const TOGGLE = document.querySelector('#mode')
 const BODY = document.querySelector('body')
 
 function toggleColorMode(){
+    let mode = localStorage.getItem('mode')
+    if(mode === 'dark'){
+        BODY.classList.add('darkmode')
+    }
     TOGGLE.addEventListener('click', () => {
         BODY.classList.toggle('darkmode')
         if(BODY.classList.contains('darkmode')){
             TOGGLE.innerHTML = '🌕'
+            localStorage.mode = 'dark'
         }else{
             TOGGLE.innerHTML = '🌑'
+            localStorage.mode = 'light'
         }
     })
 }
